6248fefda2732aef6094e610bb8289e65f77be11
NadiaCorrea <ncormor536@gmail.com>
2022-06-02 14:30:02 +0200

    commit falta terminar 6 y 9

--- ejerciciosRecuperacion/Ejer9FraseChachi.py
+++ ejerciciosRecuperacion/Ejer9FraseChachi.py
@@ -1,4 +1,6 @@
-"""9. Realizar un método que reciba una frase e informe de si es “chachi” o no. Se considera. Se considera “chachi” si las 4 primeras letras y las 4 últimas son iguales entre sí. Si es “chachi” devolverá true y false en caso contrario.
+"""9. Realizar un método que reciba una frase e informe de si es “chachi” o no. Se considera. 
+Se considera “chachi” si las 4 primeras letras y las 4 últimas son iguales entre sí. 
+Si es “chachi” devolverá true y false en caso contrario.
 Si la cadena no tiene 8 caracteres no podrá ser “chachi”.
 El número 4 debe crearse como constante de manera que el programa siga funcionando si se cambia el valor de dicha constante.
 
@@ -9,6 +11,20 @@ Ejemplo de frases “chachis”:
 En el main pedir al menos una frase al usuario y decir utilizando el metodo si es “chachi” o no."""
 
 
-
+def obtenerPrimeras(cadena):
+    result = ''
+    for i in range(0, 4):
+        result = result + cadena[i]
+    return result
+    
+    
+def obtenerUltimas (cadena): 
+    result = ''
+    for i in range (len(cadena)-4, len(cadena)):
+        result = result + cadena[i]    
+    return result
 
 frase = input("Introduce una frase: ")
+
+print(obtenerPrimeras('Manolo juega al balonmano'))
+print(obtenerUltimas('Manolo juega al balonmano'))
