14d3a289446b6234dd48bb7b68b1b315c46df7d9
NadiaCorrea <ncormor536@gmail.com>
2022-06-06 20:03:15 +0200

    commit boletin done :)

--- ejerciciosRecuperacion/Ejer26Temperatura.py
+++ ejerciciosRecuperacion/Ejer26Temperatura.py
@@ -5,4 +5,39 @@ Los días con menos temperatura
 Se lee una temperatura por teclado y se muestran los días cuya temperatura máxima 
 coincide con ella. si no existe ningún día se muestra un mensaje de información."""
 
+tempMin = []
+tempMax = []
+tempMedia = []
+diasMenos = []
+media = 0
+minima = 100
 
+for i in range(0,5):
+    diaMin = int(input("Introduce la temperatura mínima del día: "))
+    tempMin.append(diaMin)
+    if diaMin < minima:
+        minima = diaMin
+    diaMax = int(input("Introduce la temperatura máxima del día: "))
+    tempMax.append(diaMax)
+    media = (diaMax + diaMin)/2
+    tempMedia.append(media)
+
+for i in range(0, len(tempMin)): 
+    if tempMin[i] == minima:
+        diasMenos.append(i + 1)
+    
+print('Las temperaturas medias de cada día son: '+ str(tempMedia))
+print('Día(s) con menos temperatura: ' + str(diasMenos))
+
+temp = int(input("Inserta una temperatura: "))
+
+diasMax = []
+
+for i in range(0,len(tempMax)):
+    if tempMax[i] == temp:
+        diasMax.append(i + 1)
+    
+if len(diasMax) > 0:
+    print('Día(s) que coinciden con la temperatura: ' + str(diasMax))
+else:
+    print('No existe ningún día con esa temperatura.')
--- ejerciciosRecuperacion/Ejer6FactoresPrimos.py
+++ ejerciciosRecuperacion/Ejer6FactoresPrimos.py
@@ -1,5 +1,25 @@
 """ 6.- Escribir un programa que lea un número entero y lo descomponga en factores primos. 
 Por ejemplo 18 = 2  * 3 *3"""
+from setuptools._distutils.util import subst_vars
 
 num = int(input("Introduce un número: "))
 
+cociente = num
+dividendo = 2
+factoresPrimos = []
+
+while cociente != 1:
+    if cociente%dividendo == 0:
+        factoresPrimos.append(dividendo)
+        cociente = cociente//dividendo
+        dividendo = 2
+    else:
+        dividendo = dividendo + 1
+
+result = str(num) + ' = '
+for i in factoresPrimos:
+    result = result + str(i) + ' * '
+
+result = result[0:len(result)-2]
+
+print(result)
